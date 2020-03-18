e20dd20e9ede3250af33f404498e6db450babf02
phisinger <philipsinger@mail.de>
2020-03-18 08:17:53 +0700

    updated table schema

--- tables.py
+++ tables.py
@@ -4,16 +4,16 @@ import os
 from datetime import datetime
 
 
-mariadb_connection = mariadb.connect(user='root', password='1234', database='yelp_academic')
+mariadb_connection = mariadb.connect(user='root', password='1234', database='muic_restaurant_guide')
 cursor = mariadb_connection.cursor()
 
 #Create user table
 def createUserTable():
-    cursor.execute("CREATE TABLE user(user_id CHAR(22),\
+    cursor.execute("CREATE TABLE user(\
+                    user_id INT,\
                     name VARCHAR(50),\
                     review_count INT,\
-                    member_since date,\
-                    friends MEDIUMTEXT,\
+                    member_since DATETIME(),\
                     average_stars FLOAT(4),\
                     Primary Key (user_id))")
     print("User Table created") 
@@ -21,12 +21,12 @@ def createUserTable():
 #Create review table
 def createReviewTable():
     cursor.execute( "CREATE TABLE review(\
-                    review_id CHAR(22),\
-                    user_id CHAR(22),\
-                    business_id CHAR(22),\
+                    review_id INT,\
+                    user_id INT,\
+                    business_id INT,\
                     stars INT, \
-                    date CHAR(10),\
-                    text VARCHAR(20000),\
+                    date DATETIME(),\
+                    text VARCHAR(4000),\
                     PRIMARY KEY(review_id),\
                     FOREIGN KEY(user_id) REFERENCES user(user_id),\
                     FOREIGN KEY(business_id) REFERENCES business(business_id))")
@@ -35,26 +35,42 @@ def createReviewTable():
 #Create business table
 def createBusinessTable():    
     cursor.execute("CREATE TABLE business (\
-        business_id varchar(22),\
+        business_id INT,\
         name varchar(100),\
         address varchar(255),\
         city varchar(100),\
         state varchar(4),\
-        postal_code varchar(10),\
+        postal_code INT,\
         stars float(23),\
         review_count INT,\
         is_open bool,\
-        description varchar(5000)\
+        description varchar(2000)\
         hours varchar(200),\
         PRIMARY KEY (business_id))")
     print("Business Table created")
+
+#Create categories table
+def createcategoriesTable():    
+    cursor.execute("CREATE TABLE categories (\
+        business_id INT,\
+        category varchar(500),\
+        FOREIGN KEY(business_id) REFERENCES business(business_id))")
+    print("Categories Table created")
+
+#Create friends table
+def createfriendsTable():    
+    cursor.execute("CREATE TABLE friends (\
+        user_id INT,\
+        friend INT,\
+        FOREIGN KEY(user_id) REFERENCES user(user_id),\
+        FOREIGN KEY(friend) REFERENCES user(user_id)")
+    print("Friends Table created")
 
 def cleanUp():
     cursor.execute("DROP TABLE if exists review")
     cursor.execute("DROP table if exists user")
     cursor.execute("DROP TABLE if exists business")
 
-# Maybe we need a table "menu" ?
 
 def insertData():
     # function to insert after the initial insert manually by using the application
